d6e00ae4da3e59678c381f829e37e3ca1339a06a
Chloé Nogueira <nogueirach@eisti.eu>
2021-03-29 12:19:43 +0200

    Gestion des groupes, validation admin et création des dossiers.

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -1,9 +1,9 @@
 from app import api
 
 from app.resources.helloworld import HelloWorldResource, HelloWorldResourceNameToken, HelloWorldResourceNameURL, HelloWorldResourceNames
-from app.resources.register import RegisterResource
-from app.resources.validationadmin import ValidationAdminResource, ValidationAdminResourceById
-from app.resources.gestiongroupe import GestionGroupeResource
+from app.resources.Authentification.register import RegisterResource
+from app.resources.Admin.validationadmin import ValidationAdminResource, ValidationAdminResourceById
+from app.resources.Admin.gestiongroupe import GestionGroupeResource,GestionGroupeById, GestionGroupeByEleveId, UtilsateursValidesResource
 
 # Hello World
 api.add_resource(HelloWorldResource, '/api/helloworld')
@@ -15,7 +15,10 @@ api.add_resource(HelloWorldResourceNames, '/api/hello/<int:count>')
 api.add_resource(RegisterResource,'/api/register')
 
 #Gestion des groupes
+api.add_resource(UtilsateursValidesResource,'/api/utilisateursvalides')
 api.add_resource(GestionGroupeResource,'/api/groupes')
+api.add_resource(GestionGroupeById,'/api/groupes/<int:id_groupe>')
+api.add_resource(GestionGroupeByEleveId,'/api/groupesutilisateurs/<int:id_eleve>')
 
 # Validate an account
 api.add_resource(ValidationAdminResource,'/api/validation')
