3c21771c01cdd88f0227cf3491eacb7ac75a8e9d
Bolg <maxime.bolg33@gmail.com>
2021-03-29 20:27:07 +0200

    Ajouts GET/POST/PATCH/DELETE QCM
    Je dois juste encore refaire qcm d'un prof mais ça ira vite

--- backend/app/resources/qcm.py
+++ backend/app/resources/qcm.py
@@ -10,8 +10,91 @@ class QCMRessources(Resource):
         body_parser.add_argument('id',type=int,required=True,help="Pas d'identifiants renseigné.")
         args=body_parser.parse_args(strict=True)
         try :
-            qcm=Qcm.query.filter_by(id=args['id']).first()
+            qcm=db.session.query(Qcm).filter_by(id=args['id']).first()
             return get_qcm(qcm)
+        except:
+            abort(400)
+    
+    def delete(self):
+        body_parser = reqparse.RequestParser()
+        body_parser.add_argument('id',type=int,required=True,help="Pas d'identifiants renseigné.")
+        args=body_parser.parse_args(strict=True)
+        try :
+            qcm=db.session.query(Qcm).filter_by(id=args['id']).first()
+            qcmEleve=db.session.query(QcmEleve).filter_by(id_qcm=args['id']).delete()
+            for question in qcm.questions:
+                for choi in question.choix :
+                    print(choi)
+                    db.session.query(Choix).filter_by(id=choi.id).delete()
+                db.session.query(Question).filter_by(id=question.id).delete()
+            db.session.query(Qcm).filter_by(id=args['id']).delete()
+            db.session.commit()
+            return {'status':200,'message':'Le qcm a bien été supprimé.'}
+
+        except :
+            abort (400)
+    
+    def patch(self):
+        datas=request.get_json()
+        try:
+            id_qcm=datas['id']
+            titre=datas['titre']
+            groupe_id=datas['droit']['groupe']
+            eleve_id=datas['droit']['utilisateur']
+            questions=datas['questions']
+            choix=datas['choix']
+            qcm=db.session.query(Qcm).filter_by(id=id_qcm).first()
+
+            if titre != "":
+                qcm.titre=titre
+                db.session.commit()
+
+            if datas['date_debut'] != "":
+                qcm.date_debut=datetime.strptime(datas['date_debut'],"%d/%m/%Y %H:%M")
+                db.session.commit()
+
+            if datas['date_fin'] != "":
+                qcm.date_fin=datetime.strptime(datas['date_fin'],"%d/%m/%Y %H:%M")
+                db.session.commit()
+
+            if groupe_id != "":
+                db.session.query(QcmEleve).filter_by(id_qcm=qcm.id).delete()
+                add_groupe_to_qcm(groupe_id,qcm)
+
+            if eleve_id != "":
+                db.session.query(QcmEleve).filter_by(id_qcm=qcm.id).delete()
+                eleve=db.session.query(Utilisateurs).filter_by(id=eleve_id).first()
+                add_eleve_to_qcm(eleve,qcm)
+
+            if questions != "":
+                for question in questions:
+                    quest=db.session.query(Question).filter_by(id=question['id']).first()
+                    if question['titre'] != "":
+                        quest.intitule=question['titre']
+
+                    if question['ouverte'] != "":
+                        if question['ouverte'] == 0:
+                            if quest.ouverte !=0:
+                                quest.ouverte=0
+                                for choose in question['choix']: 
+                                    choixQuestion=Choix(intitule=choose['titre'],estcorrect=choose['estcorrect'],question=quest)
+                                    db.session.add(choixQuestion)
+                                    db.session.commit()
+                                db.session.commit()
+                        else:
+                            quest.ouverte=1
+                            db.session.query(Choix).filter_by(id_question=question['id']).delete()
+                        db.session.commit()
+            
+            if choix != "":
+                for choi in choix :
+                    value=db.session.query(Choix).filter_by(id=choi['id']).first()
+                    if choi['intitule'] != "" :
+                        value.intitule=choi['intitule']
+                    if choi['estcorrect'] != "":
+                        value.estcorrect=choi['estcorrect']
+                    db.session.commit()
+            return {'status':200,'message': 'Les changements ont été effectués.'}
         except:
             abort(400)
 
@@ -30,7 +113,7 @@ class QCMRessources(Resource):
                 return {'status':404,'message':'Le QCM existe déjà.'}
 
             ## On cherche le prof pour établir le lien foreign key
-            prof=Utilisateurs.query.filter_by(id=id).first()
+            prof=db.session.query(Utilisateurs).filter_by(id=id).first()
 
             ##création du QCM
             QCM=Qcm(titre=titre,date_debut=debut,date_fin=fin,utilisateurs=prof)
@@ -40,7 +123,7 @@ class QCMRessources(Resource):
             if groupe_id !="":
                 add_groupe_to_qcm(groupe_id,QCM)
             else :
-                eleve=Utilisateurs.query.filter_by(id=eleve_id).first()
+                eleve=db.session.query(Utilisateurs).filter_by(id=eleve_id).first()
                 add_eleve_to_qcm(eleve,QCM)
             
             db.session.add(QCM)
@@ -50,7 +133,7 @@ class QCMRessources(Resource):
             abort(400)
 
 def exist_qcm(titre,debut,fin,id_prof):
-    exist=Qcm.query.filter_by(titre=titre,date_debut=debut,date_fin=fin,id_professeur=id_prof).first()
+    exist=db.session.query(Qcm).filter_by(titre=titre,date_debut=debut,date_fin=fin,id_professeur=id_prof).first()
     return exist
 
 def creation_question(data,QCM):
@@ -74,12 +157,15 @@ def get_qcm(qcm):
     for question in qcm.questions:
         Listchoix=[]
         for choix in question.choix:
-            Listchoix.append({'choix':choix.intitule,'true':choix.estcorrect})
-        temp={'titre':question.intitule,'ouverte':question.ouverte,'choix':Listchoix}
+            Listchoix.append({'id':choix.id,'choix':choix.intitule,'true':choix.estcorrect})
+        temp={'id': question.id ,'titre':question.intitule,'ouverte':question.ouverte,'choix':Listchoix}
         questions.append(temp)
     date_debut=qcm.date_debut.strftime('%d/%m/%Y %H:%M')
     date_fin=qcm.date_fin.strftime('%d/%m/%Y %H:%M')
-    jsonqcm={'id':qcm.id,'titre':qcm.titre,'date_debut':date_debut,'date_fin':date_fin,'id_prof':qcm.id_professeur,'questions':questions}
+    Listusers=[]
+    for eleve in qcm.eleve:
+        Listusers.append({'id':eleve.utilisateurs.id})
+    jsonqcm={'id':qcm.id,'titre':qcm.titre,'date_debut':date_debut,'date_fin':date_fin,'id_eleves':Listusers,'id_prof':qcm.id_professeur,'questions':questions}
     return jsonqcm
 
 def add_eleve_to_qcm(eleve,QCM):
@@ -88,7 +174,7 @@ def add_eleve_to_qcm(eleve,QCM):
     db.session.commit()
 
 def add_groupe_to_qcm(groupe_id,QCM):
-    groupe=Groupe.query.filter_by(id=groupe_id).first()
+    groupe=db.session.query(Groupe).filter_by(id=groupe_id).first()
     for eleve in groupe.utilisateurs :
         add_eleve_to_qcm(eleve,QCM)
 
