25df5eafe4a4bbace1d9b5fe168ab1d05581574f
Chloé Nogueira <nogueirach@eisti.eu>
2021-03-30 11:07:19 +0200

    creation qcm à faire

---
+++ backend/app/resources/Eleve/qcmafaire.py
@@ -0,0 +1,20 @@
+from flask import request,jsonify
+from flask_restful import Resource, reqparse, abort
+from datetime import datetime
+from app import db,app
+from app.models import Qcm,Utilisateurs,Question,Choix,QcmEleve,Groupe
+from app.resources.Authentification.login import token_verif
+
+class QCMaFaireResources(Resource):
+    @token_verif
+    def get(user,self,id_eleve):
+
+
+
+
+
+
+
+def check_user_exists(id_user: str):
+    already_exists = db.session.query(db.exists().where(Utilisateurs.id == id_user)).scalar()
+    return already_exists
