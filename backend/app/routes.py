10640a31a10f370bca8c07decc3b6e4aaee3d9cd
Chloé Nogueira <nogueirach@eisti.eu>
2021-04-10 18:41:59 +0200

    Ajout post qcm eleve

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -6,6 +6,7 @@ from app.resources.Admin.gestiongroupe import GestionGroupeResource,GestionGroup
 from app.resources.qcm import QCMRessources,QCMRessourcesById
 from app.resources.Prof.gestionqcmprof import QCMProf,ListACorriger,ListACorrigerDetails
 from app.resources.Eleve.qcmafaire import QCMaFaireResources, QMCaFaireQuestionsResources
+from app.resources.Eleve.postqcmeleve import ReponsesQCM
 from app.resources.Authentification.login import LoginResource
 #tests
 
@@ -35,6 +36,9 @@ api.add_resource(ValidationAdminResource,'/api/validation')
 api.add_resource(ValidationAdminResourceById,'/api/validation/<int:id_user>')
 
 
-#qcms élèves
+#qcms à faire élèves
 api.add_resource(QCMaFaireResources,'/api/qcmaFaireInfos')
 api.add_resource(QMCaFaireQuestionsResources,'/api/qcmaFaire/<int:id_qcm_a_faire>')
+
+#réponses à un qcm
+api.add_resource(ReponsesQCM,'/api/qcmReponses')
