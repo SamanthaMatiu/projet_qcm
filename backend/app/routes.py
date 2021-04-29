7fe720676608c237098bbb48b1026ed6d1c4f8ba
Bolg <maxime.bolg33@gmail.com>
2021-04-29 14:28:14 +0200

    Ajouts fonctions

--- backend/app/routes.py
+++ backend/app/routes.py
@@ -4,11 +4,11 @@ from app.resources.Authentification.register import RegisterResource
 from app.resources.Admin.validationadmin import ValidationAdminResource, ValidationAdminResourceById
 from app.resources.Admin.gestiongroupe import GestionGroupeResource,GestionGroupeById, GestionGroupeByEleveId, ElevesValidesResource, ProfesseursValidesResource, GestionGroupeManyEleves
 from app.resources.qcm import QCMRessources,QCMRessourcesById
-from app.resources.Prof.gestionqcmprof import QCMProf,ListACorriger,ListACorrigerDetails,CorrectionDunQCM,CorrectionQuestionOuverte
+from app.resources.Prof.gestionqcmprof import QCMProf,ListACorriger,CorrectionDunQCM,CorrectionQuestionOuverte,ListQCMFait,ListQCMFaitParGroupe
 from app.resources.Eleve.qcmafaire import QCMaFaireResources, QMCaFaireQuestionsResources
-from app.resources.Eleve.qcmfait import QCMFaitResources, QCMFaitQuestionsResources
+from app.resources.Eleve.qcmfait import QCMFaitResources, QCMFaitQuestionsResources,ListQCMCorrige
 from app.resources.Eleve.postqcmeleve import ReponsesQCM
-from app.resources.Eleve.noteqcm import NoteQCM
+from app.resources.Eleve.noteqcmEleve import NoteQCM
 
 #tests
 
@@ -20,7 +20,8 @@ api.add_resource(QCMRessources,'/api/qcm')
 api.add_resource(QCMRessourcesById,'/api/qcm/<int:id_qcm>')
 api.add_resource(QCMProf,'/api/qcmProf')
 api.add_resource(ListACorriger,'/api/consultation')
-api.add_resource(ListACorrigerDetails,'/api/consultation/<int:id_qcm>/<int:id_eleve>')
+api.add_resource(ListQCMFait,'/api/listQCM')
+api.add_resource(ListQCMFaitParGroupe,'/api/listQCM/<int:id_groupe>')
 api.add_resource(CorrectionDunQCM,'/api/correction/<int:id_qcm>/<int:id_eleve>')
 api.add_resource(CorrectionQuestionOuverte,'/api/correctionQuestionOuverte/<int:id_eleve>/<int:id_question>')
 
@@ -50,4 +51,5 @@ api.add_resource(ReponsesQCM,'/api/qcmReponses')
 #qcms fait élèves
 api.add_resource(QCMFaitResources,'/api/qcmFaitInfos')
 api.add_resource(QCMFaitQuestionsResources,'/api/qcmFait/<int:id_qcm_fait>')
-api.add_resource(NoteQCM,'/api/NoteQcmFait/<int:id_eleve>/<int:id_qcm>')
+api.add_resource(NoteQCM,'/api/NoteQcmFait/<int:id_qcm>')
+api.add_resource(ListQCMCorrige,'/api/qcmCorrigeInfos')
