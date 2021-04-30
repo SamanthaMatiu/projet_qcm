dbbfe9c2c37c089d802656631035523b0e1d4e45
lamarque <lamarquema@eisti.eu>
2021-04-30 16:11:05 +0200

    Affichage liste qcms à corriger ok

--- frontend/src/components/Prof/Correction.vue
+++ frontend/src/components/Prof/Correction.vue
@@ -1,15 +1,78 @@
 <template>
-  <div>
-    <h1>correction works!</h1>
-  </div>
+  <div class="center-tab card text-center">
+    <div class="card-header"><h2>QCMs à corriger</h2></div>
+    <div class="card-body">
+      <div class="col-sm table-responsive">
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Titre</th>
+              <th scope="col">Eleve</th>
+              <th scope="col">#</th>
+            </tr>
+          </thead>
+          <tbody>
+            
+              <tr v-for="qcm in data" :key="qcm.id">
+                <td>{{ qcm.titre }}</td>
+                <td>{{ qcm.Nom }} {{ qcm.Prenom }}</td>
+                <td>
+                  <router-link :to="{ name: 'CorrigerQCM', params: { id_qcm: qcm.id_qcm, id_eleve:qcm.id_eleve }}">
+                    <i class="fas fa-pen"></i>
+                  </router-link>
+                </td>
+              </tr>
+
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>  
 </template>
 
 <script>
+  import axios from 'axios';
+
 export default {
-  name: 'Correction',
+  name: 'Consultation',
+  data() {
+      return {
+        data: []
+      }
+    },
+  methods: {
+    getQcms(){
+      const path = `http://localhost:5000/api/listQCM`;
+      axios.get(path)
+        .then((res) => {
+          this.data = res.data
+          console.log(res)
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    }
+  },
+  created() {
+    this.getQcms()
+  }
 }
 </script>
 
 <style scoped lang="scss">
 
+  .center-tab {
+    max-width: 70%;
+    left: 15%;
+  }
+
+  thead {
+    background-color: #d5deff;
+  }
+
+  .tableau {
+    text-align: center;
+    width: 70%;
+  }
 </style>
---
+++ frontend/src/components/Prof/CorrigerQcm.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="row">
+    <div class="col-1">
+      <router-link :to="{ name: 'Consultation'}">
+        <i class="retour fas fa-arrow-circle-left fa-2x"></i>
+      </router-link>
+    </div>
+    <div class="col-11">
+      <div class="card text-center">
+        <div class="card-header">{{data.titre}}</div>
+        <div class="card-body">
+          <p>
+            A faire
+          </p>
+        </div>
+        <div class="card-footer text-muted">
+          <button type="button" class="btn btn-primary btn-sm">Modifier</button>
+          <button type="button" class="btn btn-danger btn-sm" data-mdb-toggle="modal" data-mdb-target="#exampleModal">Supprimer</button>
+        </div>
+      </div>  
+    </div>
+
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button
+                          type="button"
+                          class="btn-close"
+                          data-mdb-dismiss="modal"
+                          aria-label="Close"
+                        ></button>
+                      </div>
+                      <div class="modal-body">...</div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-mdb-dismiss="modal">
+                          Close
+                        </button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios';
+
+export default {
+  name: 'CorrigerQcm',
+  data() {
+    return {
+      data: {},
+      modal: {
+        delete: false,
+        modify: false
+      }
+    }
+  },
+  methods: {
+    getQcm(){
+      const path = `http://localhost:5000/api/qcm/${this.$route.params.id}`;
+      axios.get(path)
+        .then((res) => {
+          this.data = res.data
+          console.log(res)
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    },
+    supprimerQuestion(){
+      const path = `http://localhost:5000/api/qcm`;
+      axios.delete(path)
+        .then((res) => {
+          console.log(res)
+        })
+        .catch((error) => {
+          // eslint-disable-next-line
+          console.error(error);
+        });
+    }
+  },
+  created() {
+    this.getQcm()
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+  .text-center {
+    max-width: 70%;
+    left: 12%;
+  }
+
+  .card-footer {
+    text-align: right;  
+  }
+
+  .retour {
+    color: #3f51b5;
+  }
+
+  thead {
+    background-color: #d5deff;
+  }
+
+  .tableau {
+    text-align: center;
+    width: 70%;
+  }
+</style>
+
