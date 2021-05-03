f2db961eba0e9b200cec0403746a868556d3f809
lamarque <lamarquema@eisti.eu>
2021-05-03 13:37:28 +0200

    oui

--- frontend/src/components/Prof/CorrigerQcm.vue
+++ frontend/src/components/Prof/CorrigerQcm.vue
@@ -7,62 +7,65 @@
     </div>
     <div class="col-11">
       <div class="card text-center">
-        <div class="card-header">{{data.titre}}</div>
+        <div class="card-header"><h3>{{data.titre}}</h3></div>
         <div class="card-body">
-          <p>
-            A faire
-          </p>
-        </div>
-        <div class="card-footer text-muted">
-          <button type="button" class="btn btn-primary btn-sm">Modifier</button>
-          <button type="button" class="btn btn-danger btn-sm" data-mdb-toggle="modal" data-mdb-target="#exampleModal">Supprimer</button>
+       
+            <form v-on:submit.prevent="onSubmit">
+                <div v-for="(question,id) in data.questions" :key="id">
+                <h4> {{question.intitule}} </h4>
+                <!--mdb-input v-if = question.estOuverte v-bind:key = question.choix.id disabled/>
+                <br>
+                <div v-if = !question.estOuverte >
+                    <div v-for="(choix,id_choix) in question.choix" :key="id_choix" class="justify-content-start">
+                        <div v-if = "choix.choix === qcm.reponses[id].choix">
+                            <b-form-checkbox id="choix" v-model="checkOk" disabled>{{ choix.choix }}</b-form-checkbox>
+                        </div>
+                        <div v-else>
+                            <b-form-checkbox id="choix" disabled>{{ choix.choix }}</b-form-checkbox>
+                        </div>
+                    </div>
+                    <br>
+                    
+                </div-->
+                <br>
+                </div>
+            
+            </form>
+       
         </div>
       </div>  
     </div>
-
-                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                  <div class="modal-dialog">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-                        <button
-                          type="button"
-                          class="btn-close"
-                          data-mdb-dismiss="modal"
-                          aria-label="Close"
-                        ></button>
-                      </div>
-                      <div class="modal-body">...</div>
-                      <div class="modal-footer">
-                        <button type="button" class="btn btn-secondary" data-mdb-dismiss="modal">
-                          Close
-                        </button>
-                        <button type="button" class="btn btn-primary">Save changes</button>
-                      </div>
-                    </div>
-                  </div>
-                </div>
   </div>
 </template>
 
 <script>
-//import axios from 'axios';
+import axios from 'axios';
 
 export default {
   name: 'CorrigerQcm',
   data() {
     return {
-      data: {},
-      modal: {
-        delete: false,
-        modify: false
-      }
+        id_qcm: this.$route.params.id_qcm,
+        id_eleve: this.$route.params.id_eleve,
+        data: {},
     }
   },
   methods: {
+    getData() {
+      const path = 'http://localhost:5000/api/NoteQcmFait/'+this.id_qcm+'/'+this.id_eleve;
+      axios.get(path)
+        .then((res) => {
+          this.data = res.data;
+          console.log(this.data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
 
   },
   created() {
+      this.getData();
   }
 }
 </script>
